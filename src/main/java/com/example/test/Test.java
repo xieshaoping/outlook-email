27b7f08c7cf8310f81812a5aee06035066857b61
xieshaoping <42767703+xieshaoping@users.noreply.github.com>
2020-10-10 11:14:27 +0800

    Update Test.java

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -14,9 +14,9 @@ public class Test {
         EmailConfig config = new EmailConfig();
         //发件人
         config.setPassword("Rong0424");
-        config.setUserName("xieshaoping@seczone.cn");
+        config.setUserName("*****@*****.cn");
         //收件人
-        String[] receivers = new String[]{"987319557@qq.com"};
+        String[] receivers = new String[]{"98731955*@qq.com"};
         String content = "<img class=\"fluid-img\" src=\"https://m.360buyimg.com/babel/jfs/t1/4364/2/13953/39652/5bd97a3dE32ffa419/85868d2eb0713d4e.jpg\" style=\"outline: none; -ms-interpolation-mode: bicubic; max-width: 100%; display: block; border: none;\" title=\"Radeon\" width=\"670\">";
         MailUtil.sendByOpenTraining(config,content,receivers);
     }
